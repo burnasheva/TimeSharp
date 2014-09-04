44ea8a5a477ec25f4dd3abbaf187908506bca3e1
Cody Stebbins <mail@codystebbins.com>
2014-09-04 15:50:41 -0700

    Remove homecontroller test for index

--- Timesharp.Tests/Controllers/HomeControllerTest.cs
+++ Timesharp.Tests/Controllers/HomeControllerTest.cs
@@ -9,11 +9,7 @@
 
 namespace Timesharp.Tests.Controllers
 {
-    using System.Web.Mvc;
-
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-    using Timesharp.Controllers;
 
     /// <summary>
     /// The home controller test.
@@ -21,25 +17,5 @@ namespace Timesharp.Tests.Controllers
     [TestClass]
     public class HomeControllerTest
     {
-        #region Public Methods and Operators
-
-        /// <summary>
-        /// The index.
-        /// </summary>
-        [TestMethod]
-        public void Index()
-        {
-            // Arrange
-            var controller = new HomeController();
-
-            // Act
-            var result = controller.Index() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual("Home Page", result.ViewBag.Title);
-        }
-
-        #endregion
     }
 }
